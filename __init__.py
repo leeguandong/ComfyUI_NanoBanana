2e348dd1c61fd84445ce87239afcae47714b86da
KUN <leeguandon@gmail.com>
2025-12-17 18:27:02 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI Custom Node Package Initialization
+"""
+
+from .gemini_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
